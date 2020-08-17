02c0dcc9a38043ac145ac9e56b4e9c9497904c07
the16thpythonist <jonseb1998@gmail.com>
2020-08-17 17:53:34 +0200

    update

--- ufotest/cli.py
+++ ufotest/cli.py
@@ -2,7 +2,8 @@
 import sys
 import click
 
-from ufotest.install import print_hello
+from ufotest.config import CONFIG, get_config_path
+from ufotest.install import install_dependencies
 
 
 @click.group()
@@ -11,12 +12,32 @@ def cli():
 
 
 @click.command('install')
-def install(args=None):
-    """Console script for ufotest."""
-    print_hello()
+@click.argument('path', type=click.Path(exists=True, file_okay=False, dir_okay=True, writable=True))
+@click.option('--verbose', '-v', is_flag=True)
+def install(path, verbose):
+    """Installing the project"""
+    click.echo(verbose)
+    click.secho('=====| Installing the Dependencies |=====', bold=True)
+    click.secho('Reading configuration...')
+    click.secho('- Configured OS: {}'.format(CONFIG['install']['os']))
+    click.secho('- Configured package install: {}'.format(CONFIG['install']['package_install']))
+
+    install_dependencies(verbose=verbose)
+
+    # The exit code
     return 0
 
 
+@click.command('config')
+def config():
+    """Edit the config for the ufotest CLI"""
+    config_path = get_config_path()
+    click.edit(filename=config_path)
+
+    return 0
+
+
+cli.add_command(config)
 cli.add_command(install)
 
 
--- ufotest/install.py
+++ ufotest/install.py
@@ -1,11 +1,13 @@
+import click
+import subprocess
 from ufotest.config import CONFIG
 
 
 PACKAGES = {
     'ubuntu': [
-        'python2',
-        'python2-dev',
-        'swig'
+        'swig',
+        'cmake',
+        'libxml'
     ],
     'suse': [
         'cmake',
@@ -21,11 +23,36 @@ PACKAGES = {
 }
 
 
-def install_package(package_name: str):
+def install_package(package_name: str, verbose=True):
     package_install_command = CONFIG['install']['package_install']
 
-    command = '{} {}'.format(package_install_command, package_name)
-    pass
+    click.secho('Installing package "{}"...'.format(package_name))
 
-def print_hello():
-    print("HELLO")
+    command = '{} {}'.format(package_install_command, package_name)
+    output = None if verbose else subprocess.DEVNULL
+    completed_process = subprocess.run(command, shell=True, stdout=output, stderr=output)
+
+    if completed_process.returncode == 0:
+        click.secho('Successfully installed "{}"'.format(package_name), fg='green')
+        return True
+    else:
+        click.secho('Encountered an error while installing "{}"'.format(package_name), fg='yellow')
+        return False
+
+
+def install_dependencies(verbose=True):
+    operating_system = CONFIG['install']['os']
+    packages = PACKAGES[operating_system]
+
+    installed_packages = {}
+
+    for package_name in packages:
+
+        success = install_package(package_name, verbose=verbose)
+        installed_packages[package_name] = success
+
+    successful_packages = [package_name for package_name, success in installed_packages.items() if success]
+    click.secho('Installed {} packages: {}'.format(
+        len(successful_packages),
+        ', '.join(successful_packages)
+    ), fg='green', bold=True)
