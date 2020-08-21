89e068280ab893189af3b2d10297af48821a286b
Jonas Teufel <jonseb1998@gmail.com>
2020-08-21 15:25:46 +0200

    began writing the frame.py module but realized that I definitly need the scripts from michele

--- ufotest/capture.py
+++ ufotest/capture.py
@@ -1,4 +1,13 @@
 """
 A module containing the code related to actually capturing images from the camera.
 """
-pass
+
+
+def get_frame():
+    # We are going to do all the file related operations inside the tmp folder and then the return of this function
+    # will be the file path of the image. It is then up to the caller of this function to decide where to copy this
+    # file to.
+    folder = '/tmp'
+
+    # So to get a frame you first need to do a bunch of stuff with the "pci" command...
+    pass
