5bed2be9c77ea779551c8c0b9b97416b5b5ba5fc
the16thpythonist <jonseb1998@gmail.com>
2020-09-18 12:21:50 +0200

    Added a module, which provides utilities for unittesting of the project

---
+++ ufotest/_testing.py
@@ -0,0 +1,42 @@
+import tempfile
+import os
+
+from unittest import TestCase
+
+from ufotest.util import init_install, get_path
+from ufotest.config import CONFIG, reload_config, load_config
+
+
+class UfotestTestCase(TestCase):
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        # So here I somehow have to setup a new config file etc. I guess I would do this with a temp folder?
+        cls.temp_folder = tempfile.TemporaryDirectory()
+        cls.folder_path = cls.temp_folder.name
+
+        # Now we need to set a custom path for the installation folder of ufotest, which is inside this temporary
+        # folder.
+        os.environ['UFOTEST_PATH'] = cls.folder_path
+        # After that we can install ufotest into this folder
+        init_install()
+        # After the installation we also need to reload the config dictionary for the whole system
+        reload_config()
+        cls.config = load_config()
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        # We need to properly close the temporary folder here
+        cls.temp_folder.cleanup()
+
+    # DEFAULT TESTS
+    # -------------
+
+    def test_installation_path(self):
+        installation_path = get_path()
+        expected_path = self.folder_path
+        self.assertEqual(installation_path, expected_path)
+
+    def test_config(self):
+        self.assertIn('install', self.config)
+        self.assertIn('camera', self.config)
