9886b5e8f1d687c103a775e510bbf2e6735d59b7
the16thpythonist <jonseb1998@gmail.com>
2020-09-30 12:16:46 +0200

    added a test case for powering up the camera

--- ufotest/tests/setup.py
+++ ufotest/tests/setup.py
@@ -14,15 +14,18 @@ class PowerUpTest(AbstractTest):
     def run(self):
 
         pci_write('9000', 'DFFF')
-        result1 = pci_read('9000', '1')
+        result1 = pci_read('9010', '1')
+        print(result1)
         time.sleep(0.2)
 
         pci_write('9000', 'E0FF')
-        result2 = pci_read('9000', '1')
+        result2 = pci_read('9010', '1')
+        print(result2)
         time.sleep(0.2)
 
         pci_write('9000', 'E10F')
-        result3 = pci_read('9000', '1')
+        result3 = pci_read('9010', '1')
+        print(result3)
         time.sleep(0.2)
 
         return MessageTestResult(0, 'RESULT: {}'.format(result1))
